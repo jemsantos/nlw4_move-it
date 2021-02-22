5d0dbd03ba997467b97c146df37b5f8caf867965
José Santos <71701079@aluno.faculdadecotemig.br>
2021-02-22 19:02:12 -0300

    Projeto React JS inicial

---
+++ moveit/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Hello NLW 4</h1>
+
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+export default App;
