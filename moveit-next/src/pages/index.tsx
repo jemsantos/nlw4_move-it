84da93ffe8c9d9a21f275181fa0808aa76dfb727
José Santos <71701079@aluno.faculdadecotemig.br>
2021-02-24 00:38:01 -0300

    Inseridos, CompletedChallenges e Countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,11 +1,18 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       { /* <Head> não vamos fazer deste geito...
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
@@ -18,6 +25,8 @@ export default function Home() {
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
