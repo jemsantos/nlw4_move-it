775290c8ad9cc50b58aa9e43653404c0731d5ab3
José Santos <71701079@aluno.faculdadecotemig.br>
2021-02-23 23:34:32 -0300

    Criado novo projeto com o Next. Indo até criação do Perfil

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      { /* <Head> não vamos fazer deste geito...
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+      </Head>
+
+      import Head from 'next/head';
+      */ }
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  )
+}
