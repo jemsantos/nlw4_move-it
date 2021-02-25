2c2fce148becdfd7253c1d828c50429dcbd10dcd
José Santos <71701079@aluno.faculdadecotemig.br>
2021-02-24 23:26:20 -0300

    Componente ChallengeBox desenvolvido

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
@@ -29,7 +30,9 @@ export default function Home() {
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   )
